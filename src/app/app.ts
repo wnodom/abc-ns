ac59af3ae014ca9086f232cd300705e1eba6c456
Bill Odom <bill@billodom.com>
2026-09-01 14:48:47 -0400

    Add dashboard component; add debugging styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutThisApp } from './about-this-app/about-this-app';
 import { GiveMeHelp } from './give-me-help/give-me-help';
+import { Dashboard } from './dashboard/dashboard';
 
 @Component({
-  imports: [RouterOutlet, AboutThisApp, GiveMeHelp],
+  imports: [RouterOutlet, AboutThisApp, GiveMeHelp, Dashboard],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
---
+++ src/app/dashboard/dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'ns-dashboard',
+  styleUrl: './dashboard.scss',
+  templateUrl: './dashboard.html',
+})
+export class Dashboard {}
