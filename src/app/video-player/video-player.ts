a6b2cfb510e8130c1e5b7ba854cef6bfa8c3871a
Bill Odom <bill@billodom.com>
2026-09-03 11:45:53 -0400

    Make the player play stuff

--- src/app/video-player/video-player.ts
+++ src/app/video-player/video-player.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Video } from '../video-types';
 import { JsonPipe } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   imports: [JsonPipe],
@@ -10,4 +11,16 @@ import { JsonPipe } from '@angular/common';
 })
 export class VideoPlayer {
   public readonly video = input.required<Video | undefined>();
+
+  private readonly sanitizer = inject(DomSanitizer);
+
+  protected readonly trustedYouTubeUrl = computed(() => {
+    const v = this.video();
+    if (v) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(
+        `https://www.youtube.com/embed/${v.id}`,
+      );
+    }
+    return undefined;
+  });
 }
